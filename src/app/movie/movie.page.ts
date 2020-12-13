4e34538c06d2e871591753499ece4144d855b3ab
Rodielmj <rodielm@pop-os.localdomain>
2020-12-13 12:34:38 +0100

    fixed searched list is not lost

--- src/app/movie/movie.page.ts
+++ src/app/movie/movie.page.ts
@@ -13,13 +13,15 @@ export class MoviePage {
 
   moviesData: any;
   moviesSearched: string;
-  
+
   constructor(public http: HttpClient, public movieService: MovieService) {
     this.moviesData = [];
   }
 
   ionViewWillEnter() {
-    this.getPopularMovies();
+    if (this.moviesData.length == 0) {
+      this.getPopularMovies();
+    }
   }
 
   getPopularMovies() {
@@ -37,11 +39,4 @@ export class MoviePage {
     })
   }
 
-
-
-
 }
-
-// <img class="poster lazyload lazyloaded" src="//image.tmdb.org/t/p/w300_and_h450_bestv2/apvr0s7sLvlPgNGtfuvM5EOJJMO.jpg" data-src="//image.tmdb.org/t/p/w300_and_h450_bestv2/apvr0s7sLvlPgNGtfuvM5EOJJMO.jpg" data-srcset="//image.tmdb.org/t/p/w300_and_h450_bestv2/apvr0s7sLvlPgNGtfuvM5EOJJMO.jpg 1x, //image.tmdb.org/t/p/w600_and_h900_bestv2/apvr0s7sLvlPgNGtfuvM5EOJJMO.jpg 2x" alt="" srcset="//image.tmdb.org/t/p/w300_and_h450_bestv2/apvr0s7sLvlPgNGtfuvM5EOJJMO.jpg 1x, //image.tmdb.org/t/p/w600_and_h900_bestv2/apvr0s7sLvlPgNGtfuvM5EOJJMO.jpg 2x" data-loaded="true">
-
-// url('//image.tmdb.org/t/p/w1920_and_h800_multi_faces/qPO43E0tnw9Gjh9EzkNmc3AKlE3.jpg')
